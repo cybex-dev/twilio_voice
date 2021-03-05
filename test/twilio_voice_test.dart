775b16f0e0e1614eae66fe1155d06158bc3ede14
diego <dgarcia@triploop.com>
2021-03-05 13:57:41 -0700

    removed test import

--- test/twilio_voice_test.dart
+++ test/twilio_voice_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:twilio_voice/twilio_voice.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('twilio_voice');
