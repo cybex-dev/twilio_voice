1e98e3d05f31dc10a55c9a1b2fed9bba87caaf1c
Hiren Vavadiya <vavadiyahiren@gmail.com>
2023-03-01 17:26:55 +0530

    Make Twilio voice to macOS compatible

---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,18 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import cloud_functions
+import firebase_auth
+import firebase_core
+import firebase_messaging
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseFunctionsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFunctionsPlugin"))
+  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
+}
---
+++ macos/Classes/TwilioVoicePlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface TwilioVoicePlugin : NSObject<FlutterPlugin>
+@end
