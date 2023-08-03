9fd0f5940cbef6baff7cf486748eadfe8da1d457
Charles Dyason <k4gcybex@gmail.com>
2023-08-03 04:24:52 +0200

    Fix: on page refresh, service worker page controller is null

--- example/web/load-sw.js
+++ example/web/load-sw.js
@@ -1,5 +1,14 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', async () => {
+        // https://github.com/mswjs/msw/issues/98
+        if(!navigator.serviceWorker.controller) {
+            console.warn('No service worker controller found. This page is not loaded in a service worker context. Will attempt to unregister all service workers and re-register.');
+            const registrations = await navigator.serviceWorker.getRegistrations()
+            if(registrations.length > 0) {
+                console.warn('No service worker controller found, but multiple services worker registrations are present. Unregistering...');
+                await Promise.all(registrations.map(r => r.unregister()))
+            }
+        }
         await navigator.serviceWorker.register('./twilio-sw.js').then(value => {
             console.log('Twilio Voice Service worker registered successfully.');
         }).catch((error) => {
