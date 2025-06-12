fb99e20f881caac8e16fcff98e3ce453645ba627
Charles Dyason <k4gcybex@gmail.com>
2025-06-12 19:38:21 +0200

    fix: test using matcher

--- test/twilio_voice_test.dart
+++ test/twilio_voice_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:matcher/matcher.dart' as matcher;
 
 void main() {
   const MethodChannel channel = MethodChannel('twilio_voice');
@@ -14,6 +15,7 @@ void main() {
   });
 
   test("Mock Test", () async {
-    expect(await channel.invokeMethod("42"), equals("42"));
+    final result = await channel.invokeMethod("42");
+    expect(result, matcher.equals("42"));
   });
 }
