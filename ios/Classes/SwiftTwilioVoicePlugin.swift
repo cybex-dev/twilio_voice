3fd9a59e116ae2c3784219a97329721693bacc0a
Charles Dyason <k4gcybex@gmail.com>
2025-06-16 11:28:07 +0200

    fix: remove deleting device token & binding date*
    Device Tokens are bound to the device, not per account. Thus, removing the token will require getting a new token from push registry which may take some time.
    
    See:
    - https://developer.apple.com/documentation/pushkit/supporting-pushkit-notifications-in-your-app#Generate-Push-Notifications-Using-the-Device-Token
    
    - https://developer.apple.com/documentation/pushkit/pkpushregistrydelegate/pushregistry(_:didupdate:for:)

--- ios/Classes/SwiftTwilioVoicePlugin.swift
+++ ios/Classes/SwiftTwilioVoicePlugin.swift
@@ -499,10 +499,10 @@ public class SwiftTwilioVoicePlugin: NSObject, FlutterPlugin,  FlutterStreamHand
                 self.sendPhoneCallEvents(description: "LOG|Successfully unregistered from VoIP push notifications.", isError: false)
             }
         }
-        UserDefaults.standard.removeObject(forKey: kCachedDeviceToken)
+        //UserDefaults.standard.removeObject(forKey: kCachedDeviceToken)
         
         // Remove the cached binding as credentials are invalidated
-        UserDefaults.standard.removeObject(forKey: kCachedBindingDate)
+        //UserDefaults.standard.removeObject(forKey: kCachedBindingDate)
     }
     
     /**
