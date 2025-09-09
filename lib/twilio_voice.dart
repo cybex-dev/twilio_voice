70b58b4971bb0f21f6e3c305da3240781abdb105
Charles Dyason <k4gcybex@gmail.com>
2025-09-09 16:10:03 +0200

    feat: expose twilio_voice platform

--- lib/twilio_voice.dart
+++ lib/twilio_voice.dart
@@ -4,6 +4,7 @@ import 'package:twilio_voice/_internal/method_channel/twilio_call_method_channel
 
 import '_internal/method_channel/twilio_voice_method_channel.dart';
 
+export '_internal/platform_interface/twilio_voice_platform_interface.dart' show TwilioVoicePlatform;
 export './models/active_call.dart';
 export './models/call_event.dart';
 
